c4e10b6c8e57980b8558780fef275c12e9ce9cfb
Abdul Samad <samad028@d.umn.edu>
2019-12-01 14:51:25 -0600

    Corrected imageaiweighs path

--- paths.py
+++ paths.py
@@ -1,7 +1,7 @@
 KERNELS_DIR = '../kernels/'
 MODELS_DIR = '../models/'
 DATASET_DIR = '../dataset/'
-IMAGEAIWEIGHTS = '../imageaiweighs/'
 
+IMAGEAIWEIGHTS = MODELS_DIR + 'imageaiweighs/'
 SUBMISSION_DIR = KERNELS_DIR + 'submissions/'
 TEST_IMAGES_DIR = DATASET_DIR + 'test_images/'
