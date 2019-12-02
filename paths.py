676dd2e1dde56bdb505c58b3bb0529fa7330fccf
Abdul Samad <samad028@d.umn.edu>
2019-12-01 18:01:21 -0600

    Basic Analysis done for detection with diff probab and detectors

--- paths.py
+++ paths.py
@@ -1,7 +1,9 @@
 KERNELS_DIR = '../kernels/'
 MODELS_DIR = '../models/'
 DATASET_DIR = '../dataset/'
+DETECTIONS_DIR = '../detections/'
 
 IMAGEAIWEIGHTS = MODELS_DIR + 'imageaiweighs/'
+ANALYSIS_DIR = KERNELS_DIR + 'analysis/'
 SUBMISSION_DIR = KERNELS_DIR + 'submissions/'
 TEST_IMAGES_DIR = DATASET_DIR + 'test_images/'
