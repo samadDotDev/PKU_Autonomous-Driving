9956fe14dcfe730f2e59755c7ab435eb570e6457
Abdul Samad <samad028@d.umn.edu>
2019-12-02 19:03:34 -0600

    Reorganized dirs to bring lightweight models within git

--- paths.py
+++ paths.py
@@ -1,9 +1,15 @@
-KERNELS_DIR = '../kernels/'
-MODELS_DIR = '../models/'
-DATASET_DIR = '../dataset/'
-DETECTIONS_DIR = '../detections/'
+# These are present over git
 
-IMAGEAIWEIGHTS = MODELS_DIR + 'imageaiweighs/'
+KERNELS_DIR = '../kernels/'
+
+MODELS_DIR = KERNELS_DIR + 'models/'
 ANALYSIS_DIR = KERNELS_DIR + 'analysis/'
 SUBMISSION_DIR = KERNELS_DIR + 'submissions/'
+
+# The following are too heavy to push to github
+
+DATASET_DIR = '../dataset/'
+DETECTIONS_DIR = '../detections/'
+IMAGEAIWEIGHTS = '../imageaiweighs/'
+
 TEST_IMAGES_DIR = DATASET_DIR + 'test_images/'
